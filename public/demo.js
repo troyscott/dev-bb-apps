3956cb9b7ffd5729ff5517a50f2e6af5b720ce3d
Troy Scott <troys2005@gmail.com>
2014-08-19 19:42:00 -0700

    Ch. 2- corrected error in backbone template. This commit runs with no errors

--- public/demo.js
+++ public/demo.js
@@ -2,46 +2,16 @@
  
 var ToDo = Backbone.Model.extend({
   defaults: {
-    
-    
-    
+    title: '',
+    completed: false
   }
   
 });
 
-// Create a new ToDo object
 
-var myToDo = new ToDo({
-  
-  title: 'Check attributes property of the logged models in the console'
-  
-  
-});
+// Create a ToDo
 
-// Views
-var ToDoView = Backbone.View.extend({
-  
-  tagname: 'li',
-  todoTpl: _.template($('#item-template').html()),
-  
-  events: {
-   'dblclick label': 'edit',
-   'keypress .edit': 'updateOnEnter',
-   'blur .edit': 'close
-  
-  },
-
-  // Call wehn the view is first created
-initialize: funtion() {
-  this.$el = $('#todo');
-  
-  
-  
-},
-
-
-                                    
-                                    
-  
+var myTodo = new ToDo({
+  title: 'Check the console dude'
   
 });
